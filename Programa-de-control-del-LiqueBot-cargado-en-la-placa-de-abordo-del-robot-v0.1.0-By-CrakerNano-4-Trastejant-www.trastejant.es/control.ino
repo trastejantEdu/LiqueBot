d124b00cc7e4bc517d57eb6038266f110ea8a829
Trastejant <crakernano@gmail.com>
2014-04-03 14:36:29 +0200

    Update control.ino
    Añadida funcionalidad para leer los sensores de humedad, temperatura,  Monoxido de Carbono y Dioxido de Carbono

--- Programa-de-control-del-LiqueBot-cargado-en-la-placa-de-abordo-del-robot-v0.1.0-By-CrakerNano-4-Trastejant-www.trastejant.es/control.ino
+++ Programa-de-control-del-LiqueBot-cargado-en-la-placa-de-abordo-del-robot-v0.1.0-By-CrakerNano-4-Trastejant-www.trastejant.es/control.ino
@@ -10,18 +10,29 @@
                                                                     www.trastejant.es
 */
 
-#include <Servo.h>
+#include <Servo.h> //Llamada a la libreria Servo
+#include "DHT.h"   //Llamada a la libreria DHT(Sensor de humedad)  
 
+//Configuracion del sensor de humedad
+#define DHTPIN 6   //Declaramos que el sensor esta en el pin 6      
+#define DHTTYPE DHT11 //Definimos el modelo del sensor        
+DHT dht(DHTPIN, DHTTYPE); //Definimos la variable que utilizaremos para comunicarnos con el sensor    
+
+//Declaracion de objetos tipo servo
 Servo servoL;
 Servo servoR;
 Servo direccion;
 
 void setup(){
+
+  dht.begin(); //Inicializamos el sensor de Humedad
   
+  //Declaracion de pines de los servos
   servoR.attach(9);
   servoL.attach(10);
   direccion.attach(11);
   
+  //Posiciones iniciales de los servos
   direccion.write(90);
   servoL.write(90);
   servoR.write(90);
@@ -68,4 +79,20 @@ void izquierda(){
 void centrar(){
   direccion.write(90);
   delay(10);
+}
+
+float leerTemperatura(){
+return dht.readTemperature();     
+}
+
+float leerHumedad(){
+return dht.readHumidity();   
+}
+
+float leerMonoxido(int sensor){
+return analogRead(sensor);
+}
+
+float leerDioxido(int sensor){
+return analogRead(sensor);
 }
